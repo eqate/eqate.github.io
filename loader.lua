fd75fba2980b913e350b14c426ffbfa9dd370f4f
eqate <166958338+eqate@users.noreply.github.com>
2025-02-21 19:38:09 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,9 @@
 if game.GameId == 7030416430 then
     loadstring(game:HttpGet("https://eqate.github.io/slipperstairs.lua", true))()
-elseif game.GameId == 65241 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyGodCheesey/Galaxy-Hub/main/NDS.lua", true))()
+elseif game.GameId == 5830262966 then
+    loadstring(game:HttpGet("https://eqate.github.io/glassbridge-squidgame.lua", true))()
+elseif game.GameId == 789229256 then
+    loadstring(game:HttpGet("https://eqate.github.io/placeholfder.lua", true))()
 else
     loadstring(game:HttpGet("https://eqate.github.io/not-supported.lua", true))()
 end

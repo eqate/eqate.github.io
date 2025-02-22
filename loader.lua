7b7e30907c0dbe241c50dd52dc7742e747f8d258
eqate <166958338+eqate@users.noreply.github.com>
2025-02-21 19:29:14 -0500

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,7 @@
+if game.GameId == 873703865 then
+    loadstring(game:HttpGet("https://eqate.github.io/slipperstairs.lua", true))()
+elseif game.GameId == 65241 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyGodCheesey/Galaxy-Hub/main/NDS.lua", true))()
+else
+    loadstring(game:HttpGet("https://eqate.github.io/not-supported.lua", true))()
+end
